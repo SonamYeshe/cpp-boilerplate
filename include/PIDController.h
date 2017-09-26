16332fcc52350b92a6ed34d045e548892b2c739a
Jovi <380081356@qq.com>
2017-09-26 13:23:35 -0700

    //	modified:   include/PIDController.h //	modified:   test/PIDControllerTest.cpp

--- include/PIDController.h
+++ include/PIDController.h
@@ -8,38 +8,47 @@
 #ifndef INCLUDE_PIDCONTROLLER_H_
 #define INCLUDE_PIDCONTROLLER_H_
 
-class PIDController{
+// Design a PIDController class to computes
+// a new velocity given a target setpoint and
+// actual velocity.
+
+class PIDController {
 private:
-	double Kp=3;
-	double Ki=0.4;
-	double Kd=0.5;
-	double intig=0;
+// Set the control gain to be some constants
+	double Kp = 3;
+	double Ki = 0.4;
+	double Kd = 0.5;
+// Assuming the intig value to be 0, in other
+// words, there's no accumulated intig so far.
+	double intig = 0;
 public:
-	double computeNewVelocity(double TarSetPoint, double ActVelocity, double T)
-	{
-	double error=TarSetPoint-ActVelocity;
-	double deriv=0;
-	intig = error + intig;
-	deriv = error / T;
+	double computeNewVelocity(double TarSetPoint, double ActVelocity,
+			double T) {
+		double error = TarSetPoint - ActVelocity;
+// Only intig need to be accumulated, deriv &
+// propotional don't
+		double deriv = 0;
+		intig = error + intig;
+		deriv = error / T;
 
-	if (TarSetPoint >0)
-		{if (Kp*error + Ki*intig + Kd*deriv > 2 * TarSetPoint)
-			return 2 * TarSetPoint;
-		else
-			return Kp*error + Ki*intig + Kd*deriv;}
-	else{
-		if (Kp*error + Ki*intig + Kd*deriv < 2 * TarSetPoint)
-			return 2 * TarSetPoint;
-		else
-			return Kp*error + Ki*intig + Kd*deriv;}
-
-	};
-
-
+// Separate the cases to be motor rotates clockwisely
+// & counterclockwisely
+		if (TarSetPoint > 0) {
+// Set 2 times of TarSetPoint to be the upper limit
+// to protect the motor
+			if (Kp * error + Ki * intig + Kd * deriv > 2 * TarSetPoint)
+				return 2 * TarSetPoint;
+			else
+				return Kp * error + Ki * intig + Kd * deriv;
+		} else {
+			if (Kp * error + Ki * intig + Kd * deriv < 2 * TarSetPoint)
+				return 2 * TarSetPoint;
+			else
+				return Kp * error + Ki * intig + Kd * deriv;
+		}
+	}
+	;
 
 };
-
-
-
 
 #endif /* INCLUDE_PIDCONTROLLER_H_ */
--- test/PIDControllerTest.cpp
+++ test/PIDControllerTest.cpp
@@ -7,14 +7,24 @@
 #include <gtest/gtest.h>
 #include "PIDController.h"
 
+// Give a positive input for velocity, assuming motor
+// rotates clockwisely, To test if the result returns
+// the upper limit in order to protect the motor.
+
 TEST(PIDTest, pass) {
- PIDController PID;
- double targetPoint=100;
- EXPECT_NEAR(2*targetPoint, PID.computeNewVelocity(targetPoint,0,10),0.5);
+	PIDController PID;
+	double targetPoint = 100;
+	EXPECT_NEAR(2 * targetPoint, PID.computeNewVelocity(targetPoint, 0, 10),
+			0.5);
 }
 
+// Give a negative input for velocity, assuming motor
+// rotates counterclockwisely, To test if the result returns
+// the upper limit in order to protect the motor.
+
 TEST(PIDTest2, pass) {
- PIDController PID;
- double targetPoint=(-100);
- EXPECT_NEAR(2*targetPoint, PID.computeNewVelocity(targetPoint,-10,10),0.5);
+	PIDController PID;
+	double targetPoint = (-100);
+	EXPECT_NEAR(2 * targetPoint, PID.computeNewVelocity(targetPoint, -10, 10),
+			0.5);
 }
